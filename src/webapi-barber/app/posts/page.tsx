3c68fc4e98e19ee1a6d8658c64bf8100eabe7143
kardec.dev <kardec.dev@gmail.com>
2026-01-15 17:45:33 -0300

    feat: add button component and utility functions, update styles and dependencies

--- src/webapi-barber/app/posts/page.tsx
+++ src/webapi-barber/app/posts/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import { Button } from "@/app/_components/ui/button"
 
 export default function FirstPost() {
   return (
@@ -6,11 +6,9 @@ export default function FirstPost() {
               <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <h1 className="text-3xl font-bold mb-4 text-red-500">First Post</h1>
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-                  <Link
-                    href="/"
-                    className="mt-10 rounded-full bg-blue-600 px-6 py-3 text-white hover:bg-blue-700">
-                    Back to home
-                  </Link>
+                  <Button variant="outline">
+                    Back to Home 
+                  </Button>
                 </div>
               </main>
             </div>
